78336a7ab27e74aa85584fd118a57a6ce7afe20e
ckt22 <tangchekkit@kidpairing.com>
2022-01-14 22:37:44 +0800

    tags update

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Cheuk Kit TANG',
   shortName: 'Cheuk Kit TANG',
   description: 'This is my software engineering portfolio, with my projects, internships, and few of my favourite things.',
-  url: 'https://https://cktangad.student.ust.hk/', // Domain of your site without prefix!
+  url: 'https://ckt22.github.io/portfolio-revamp/', // Domain of your site without prefix!
   prefix: '/',
   author: 'Cheuk Kit Tang'
 }
