a5ff0e07ab03d140df95817dbfdf72530cd7e03f
ckt22 <tangchekkit@kidpairing.com>
2022-02-18 20:58:51 +0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-37c3297698f3dd0ac440.js"
+    "url": "webpack-runtime-657b088aafaa83565c89.js"
   },
   {
     "url": "styles.15527154dcd204931442.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4c3c45f17be48c52043304d873713602"
+    "revision": "63c2abb08ad38967320b74284d418dd5"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -53,7 +53,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "8c3d1809b05fc457079384b01720f0c8"
+    "revision": "8b7e6453065adcfcedeb2fba950d13ec"
   },
   {
     "url": "polyfill-3343f0d7da5cea86518b.js"
